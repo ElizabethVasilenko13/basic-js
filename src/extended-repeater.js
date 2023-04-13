f6948cf22715d04835b315e7956e880e23269fba
Yelyzaveta Vasylenko <liza.vasilenko13@gmail.com>
2023-04-13 20:27:54 +0200

    feat: add repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options) {
+
+  let res = [];
+
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      res.push(String(str));
+    }
+  } else {
+    res.push(str);
+  }
+
+  if (!options.separator) {
+    options.separator = '+'
+  } 
+
+  if (!options.additionSeparator) {
+    options.additionSeparator = '|'
+  }
+
+  if (options.addition || typeof options.addition === 'boolean' || typeof options.addition === 'object') {
+    options.addition = String(options.addition);
+    for (let i = 0; i < res.length; i++) {
+      if (!options.additionRepeatTimes) {
+        res[i] = res[i] + options.addition;
+      }
+      for (let j = 0; j < options.additionRepeatTimes; j++) {
+        if (options.additionSeparator) {
+          res[i] = res[i] + options.addition + options.additionSeparator;
+        } else {
+          res[i] = res[i] + options.addition;
+        }
+      } 
+      if (options.additionSeparator && options.additionRepeatTimes) {
+        res[i] = res[i].substring(0, res[i].length - options.additionSeparator.length)
+      }
+    } 
+  }
+
+  return res.join(options.separator);
+
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,12 +12,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+/* class DepthCalculator {
+  calculateDepth(arr) {
+    let count = 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        count += this.calculateDepth(arr[i]);
+        count++;
+      }
+    }
+    return count;
   }
 }
+
+const instance = new DepthCalculator();
+console.log(instance.calculateDepth([1, 2, 3, [1], 4, 5, [1]])); // 2
+console.log(instance.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []])); // 4
+console.log(instance.calculateDepth([1, 2, 3, 4, 5, [1]])); // 2 */
+
+// function calculateDepth(arr) {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//       if (typeof arr[i] === 'object') {
+//         count += calculateDepth(arr[i]);
+//         count++;
+//       }
+//     }
+//     return count;
+// }
+
+// console.log(calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]));
 
 module.exports = {
   DepthCalculator
