68aa7633a336369bf3f68e85b3c507a7d486fea0
Yelyzaveta Vasylenko <liza.vasilenko13@gmail.com>
2023-04-15 15:55:40 +0200

    feat: add task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,22 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-/* class DepthCalculator {
+ class DepthCalculator {
   calculateDepth(arr) {
-    let depth = 1;
-    for (let i = 0; i < arr.length; i++) {
-      if (Array.isArray(arr[i])) {
-        depth = Math.max(depth, 1 + this.calculateDepth(arr[i]));
-      }
-    }
-    return depth;
+    // let depth = 1;
+    // for (let i = 0; i < arr.length; i++) {
+    //   if (Array.isArray(arr[i])) {
+    //     depth = Math.max(depth, 1 + this.calculateDepth(arr[i]));
+    //   }
+    // }
+    // return depth;
   }
 }
 
 const instance = new DepthCalculator();
 console.log(instance.calculateDepth([1, 2, 3, [1], 4, 5, [1]])); // 2
 console.log(instance.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []])); // 4
-console.log(instance.calculateDepth([1, 2, 3, 4, 5, [1]])); // 2  */
+console.log(instance.calculateDepth([1, 2, 3, 4, 5, [1]])); // 2  
 
 module.exports = {
   DepthCalculator
